44cfd4faa4cbc542ab23b99afa654efa353c16db
9898s <sueh9898@icloud.com>
2023-08-11 05:57:52 +0900

    :white_check_mark: void 롬복_기능_테스트() 테스트 추가

---
+++ src/test/java/com/example/springboot/dto/HelloResponseDtoTest.java
@@ -0,0 +1,21 @@
+package com.example.springboot.dto;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class HelloResponseDtoTest {
+    @Test
+    void 롬복_기능_테스트() {
+        // given
+        String name = "test";
+        int amount = 1000;
+
+        // when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertEquals(dto.getName(), name);
+        assertEquals(dto.getAmount(), amount);
+    }
+}
